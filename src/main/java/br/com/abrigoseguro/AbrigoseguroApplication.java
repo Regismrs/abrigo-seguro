4a01542a3622ff408771fd7e1e123a29f6575a27
regismrs <regismrs@hotmail.com>
2025-06-30 21:25:58 -0300

    initial commit

---
+++ src/main/java/br/com/abrigoseguro/AbrigoseguroApplication.java
@@ -0,0 +1,13 @@
+package br.com.abrigoseguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbrigoseguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbrigoseguroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abrigoseguro/AbrigoseguroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrigoseguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbrigoseguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
